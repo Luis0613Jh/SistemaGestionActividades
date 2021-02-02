0fd123cc6246a4c857bbfa8b94eaaff1208b147e
Luis Jh <luis0613jh@gmail.com>
2021-02-02 18:34:09 -0500

    Se creó la clase VerificarCedula, y se creó los metodos de expresiones regulares en Utilidades.controlador

--- src/controlador/utilidades/UtilidadesControlador.java
+++ src/controlador/utilidades/UtilidadesControlador.java
@@ -196,4 +196,24 @@ public class UtilidadesControlador {
             cdx.addItem(dato[i]);
         }
     }
+    
+    public static boolean validarNumeroEntero(String numero) {
+        return numero.matches("^[-]?[0-9]+$");
+    }
+    
+    public static boolean validarNumeroDouble(String numero) {
+        return numero.matches("^[-]?[0-9]+([.][0-9]+)?$");
+    }
+    
+    public static boolean validarCadenaSoloLetras(String cadena) {
+        return cadena.matches("^([A-Za-z]+[ ]?)+$");
+    }
+    
+    public static boolean validarNombre(String nombre) {
+        return nombre.matches("^(([A-Za-z]{1})?[a-z]+[ ]?)+$");
+    }
+
+    public static boolean validarCedula(String cedula) {
+        return cedula.matches("^[0-9]{10}$");
+    }
 }
