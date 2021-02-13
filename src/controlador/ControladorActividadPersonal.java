2de6b9755aaa43d72e58dee655c6bdb76dc81a23
Patricio <patricparedes18@gmail.com>
2021-02-13 00:48:25 -0500

    Culminacion logica vista ActividadPersonal
    Finaliza dicha vista

--- src/controlador/ControladorActividadPersonal.java
+++ src/controlador/ControladorActividadPersonal.java
@@ -1,6 +1,43 @@
 
 package controlador;
 
+import controlador.DAO.objetosDAO.PersonaDAO;
+import controlador.listaSimple.ListaSimple;
+import controlador.servicio.ActividadPersonalServicio;
+import javax.swing.JOptionPane;
+import modelo.ActividadPersonalModelo;
+
 public class ControladorActividadPersonal {
+    private ActividadPersonalModelo actividad;
+
+    public ActividadPersonalModelo getActividad() {
+        if(actividad == null){
+            actividad = new ActividadPersonalModelo();
+        }
+        return actividad;
+    }
+
+    public void setActividad(ActividadPersonalModelo actividad) {
+        this.actividad = actividad;
+    }
     
+    public boolean guardarActividadPersonal(){
+        try {
+            ActividadPersonalServicio servicio = new ActividadPersonalServicio();
+            servicio.setActividadPersonal(actividad);
+            if(servicio.guardarActividadPersonal()){
+                return true;
+            }else{
+                return false;
+            }            
+        } catch (Exception e) {
+            System.out.println(" Error al insertar ");
+            return false;
+        }
+    }
+    public int numeroEmpleados() {
+        ActividadPersonalServicio servicio = new ActividadPersonalServicio();
+        ListaSimple lista = servicio.listarActividadesPersonales();
+        return lista.tamanio();
+    }
 }
